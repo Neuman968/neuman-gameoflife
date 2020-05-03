76406c6b8f38aa7eaa80b74717d7356b00faef53
johnflynn <johnflynn493@gmail.com>
2020-05-03 18:35:59 -0400

    Added basic 10 x 10 grid display

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Grid from "@material-ui/core/Grid";
+import CellRow from './components/CellRow';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const arr = [];
+    for (let i = 0; i < 100; i++) {
+        arr.push(i);
+    }
+
+    const rows = [...Array(100 / 10).keys()]
+        .map((idx) => {
+            const rowCells = (idx * 10);
+            const startIdx = rowCells;
+            const endIdx = startIdx + 10;
+            return <Grid container spacing={0} xs={1}>
+                <CellRow cells={arr.slice(startIdx, endIdx)}/>
+            </Grid>
+        });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {rows}
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/CellRow.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+
+const CellRow = (props) => {
+    return props.cells.map((cellVal) => {
+        return <Grid item xs={1}>
+            .
+        </Grid>
+    })
+};
+
+
+export default CellRow;
