85a3081b0281c4e6f6d38e426da8353b66240121
johnflynn <johnflynn493@gmail.com>
2020-05-05 21:50:58 -0400

    Lol fuck you font-size!!!

--- src/components/CellRow.js
+++ src/components/CellRow.js
@@ -1,13 +1,13 @@
 import React from "react";
 import classes from './CellRow.module.css'
 
-const deadCell = <rect width="10" height="10" style={{
+const deadCell = <rect width="10px" height="10px" style={{
     fill: "#FF3412",
     // strokeWidth: 3,
     stroke: "#FF3412"
 }}/>
 
-const aliveCell = <rect width="10" height="10" style={{
+const aliveCell = <rect width="10px" height="10px" style={{
     fill: "#91F5FF",
     // strokeWidth: 3,
     stroke: "#91F5FF"
@@ -16,7 +16,7 @@ const aliveCell = <rect width="10" height="10" style={{
 const CellRow = (props) => {
     return props.cells.map((cellVal) => {
         return <div className={classes.Cell}>
-            <svg width="10" height="10">
+            <svg width="10px" height="10px">
                 { cellVal % 2 !== 0 ? aliveCell : deadCell}
             </svg>
         </div>
