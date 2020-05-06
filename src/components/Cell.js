c6c5e8237fca552bc3479acaf438cf586f64ffdf
johnflynn <johnflynn493@gmail.com>
2020-05-05 22:15:39 -0400

    Added some more syling, broke out Cell into it's own component.

---
+++ src/components/Cell.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './Cell.module.css'
+
+const deadCell = <rect width="10px" height="10px" style={{
+    fill: "#FF3412",
+    // strokeWidth: 3,
+    stroke: "#FF3412"
+}}/>
+
+const aliveCell = <rect width="10px" height="10px" style={{
+    fill: "#91F5FF",
+    // strokeWidth: 3,
+    stroke: "#91F5FF"
+}}/>
+
+
+const Cell = (props) => {
+    return (<div className={classes.Cell}>
+        <svg width="10px" height="10px">
+            deadCell
+            {/*{props.cellVal % 2 !== 0 ? aliveCell : deadCell}*/}
+        </svg>
+    </div>)
+};
+
+export default Cell;
--- src/components/CellRow.js
+++ src/components/CellRow.js
@@ -1,25 +1,9 @@
 import React from "react";
-import classes from './CellRow.module.css'
-
-const deadCell = <rect width="10px" height="10px" style={{
-    fill: "#FF3412",
-    // strokeWidth: 3,
-    stroke: "#FF3412"
-}}/>
-
-const aliveCell = <rect width="10px" height="10px" style={{
-    fill: "#91F5FF",
-    // strokeWidth: 3,
-    stroke: "#91F5FF"
-}}/>
+import Cell from './Cell'
 
 const CellRow = (props) => {
     return props.cells.map((cellVal) => {
-        return <div className={classes.Cell}>
-            <svg width="10px" height="10px">
-                { cellVal % 2 !== 0 ? aliveCell : deadCell}
-            </svg>
-        </div>
+        return <Cell cellVal={cellVal}/>
     })
 };
 
