1d689ffbbe15042d81e529a59f40be70e1387a00
johnflynn <johnflynn493@gmail.com>
2020-05-05 20:52:02 -0400

    Wip using react muiTable.. not the best due to table cell padding...

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import './App.css';
-import Grid from "@material-ui/core/Grid";
 import CellRow from './components/CellRow';
+import Table from "@material-ui/core/Table";
+import TableRow from "@material-ui/core/TableRow";
+
+const L = 10;
+
+const W = 10;
 
 function App() {
 
-
     const arr = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < L * W; i++) {
         arr.push(i);
     }
 
-    const rows = [...Array(100 / 10).keys()]
+    const rows = [...Array(W).keys()]
         .map((idx) => {
-            const rowCells = (idx * 10);
+            const rowCells = (idx * L);
             const startIdx = rowCells;
-            const endIdx = startIdx + 10;
-            return <Grid container spacing={0} xs={1}>
+            const endIdx = startIdx + L;
+            return <TableRow>
                 <CellRow cells={arr.slice(startIdx, endIdx)}/>
-            </Grid>
+            </TableRow>
         });
 
     return (
         <div className="App">
             <header className="App-header">
-                {rows}
+                <Table size="small">
+                    {rows}
+                </Table>
             </header>
         </div>
     );
--- src/components/CellRow.js
+++ src/components/CellRow.js
@@ -1,11 +1,25 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
+import TableCell from '@material-ui/core/TableCell';
+
+const deadCell = <rect width="300" height="100" style={{
+    fill: "#FF3412",
+    strokeWidth: 3,
+    stroke: "#FF3412"
+}}/>
+
+const aliveCell = <rect width="300" height="100" style={{
+    fill: "#91F5FF",
+    strokeWidth: 3,
+    stroke: "#91F5FF"
+}}/>
 
 const CellRow = (props) => {
     return props.cells.map((cellVal) => {
-        return <Grid item xs={1}>
-            .
-        </Grid>
+        return <TableCell>
+            <svg width="10" height="10">
+                { cellVal % 2 !== 0 ? aliveCell : deadCell}
+            </svg>
+        </TableCell>
     })
 };
 
