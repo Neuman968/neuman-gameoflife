715304e27ec45d46e310da23ae0144b2ab1a8532
johnflynn <johnflynn493@gmail.com>
2020-05-05 22:18:50 -0400

    Added a fun debug handler for clicking and retrieving index of Cell.js.

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -13,9 +13,12 @@ const aliveCell = <rect width="10px" height="10px" style={{
     stroke: "#91F5FF"
 }}/>
 
+const debugConsole = (cellVal) => {
+    console.log("index is " + cellVal)
+}
 
 const Cell = (props) => {
-    return (<div className={classes.Cell}>
+    return (<div className={classes.Cell} onClick={() => debugConsole(props.cellVal)}>
         <svg width="10px" height="10px">
             deadCell
             {/*{props.cellVal % 2 !== 0 ? aliveCell : deadCell}*/}
