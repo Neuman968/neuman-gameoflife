ab40c24de1c3575b7765562fa5b99e14ae82c591
johnflynn <johnflynn493@gmail.com>
2020-05-16 22:20:25 -0400

    reduced viewport to 8px

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 import classes from './Cell.module.css'
 
-const deadCell = <rect width="10px" height="10px" style={{
+const deadCell = <rect width="8px" height="8px" style={{
     fill: "rgba(75,76,73,0.96)",
     // strokeWidth: 3,
     stroke: "rgba(75,76,73,0.96)"
 }}/>
 
-const aliveCell = <rect width="10px" height="10px" style={{
+const aliveCell = <rect width="8px" height="8px" style={{
     fill: "#ff1200",
     // strokeWidth: 3,
     stroke: "#ff6700"
@@ -27,7 +27,7 @@ const Cell = (props) => {
     }
 
     return (<div className={classesList.join(' ')} onClick={() => aliveHandler()}>
-        <svg width="10px" height="10px">
+        <svg width="8px" height="8px">
             {
                 cellState
             }
