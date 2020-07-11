a560c2f0e95a04b4f116a0f0cf3840bbe4201cf8
johnflynn <johnflynn493@gmail.com>
2020-07-11 18:25:14 -0400

    Wip on correct row column ordering issue.

--- src/components/Simulation.js
+++ src/components/Simulation.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import CellWorld from "./CellWorld";
 import WorldControls from "./WorldControls";
+import Button from "@material-ui/core/Button";
 
 export const getCellKey = (row, column) => row + '-' + column;
 
@@ -92,6 +93,7 @@ const Simulation = () => {
             cellstate={cellstate}
             updaterunning={updaterunning}
         />
+        <Button onClick={() => console.log(cellstate.aliveCells)}>Print state</Button>
     </>);
 }
 
