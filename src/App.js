31e0e5e140f255a9a1e87026b99001196b25e0e7
johnflynn <johnflynn493@gmail.com>
2020-07-15 23:03:38 -0400

    Working on making themeing..

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
     return (
         <div className="App">
-            <div className="App-header">
+            <div>
                 <Simulation/>
             </div>
         </div>
