22cc0f2443a3ab893d9b318350dbeb7b285b998e
johnflynn <johnflynn493@gmail.com>
2020-07-15 23:19:01 -0400

    Added dark theming.. or more media query to get if the user prefers dark theme or light theme.. this is cool because we can potentially use this as a light/dark theme toggle.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React from 'react';
 import './App.css';
 import Simulation from "./components/Simulation";
 
+import {createMuiTheme} from '@material-ui/core/styles';
+import {useMediaQuery} from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import {ThemeProvider} from "@material-ui/styles";
+
 function App() {
+
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = React.useMemo(
+        () =>
+            createMuiTheme({
+                palette: {
+                    type: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+
 
     return (
         <div className="App">
-            <div>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
                 <Simulation/>
-            </div>
+            </ThemeProvider>
         </div>
     );
 }
