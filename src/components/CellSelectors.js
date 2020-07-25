c9807b5874b66885d8fd1db95f1154b650d54669
johnflynn <johnflynn493@gmail.com>
2020-07-25 17:37:43 -0400

    Added glider cell selector. Added mapToCellKey function that handles mapping array or row-col arrays to a flat array of cell keys.

--- src/components/CellSelectors.js
+++ src/components/CellSelectors.js
@@ -7,6 +7,8 @@ export const getSelectorFunc = (cellSelector) => {
             return singleSquareSelector
         case 2:
             return barSelector
+        case 3:
+            return gliderSelector
     }
 }
 
@@ -14,5 +16,15 @@ const singleSquareSelector = (selectedIdx) => [selectedIdx]
 
 const barSelector = (selectedIdx) => {
     const [row, col] = rowColFromCelKey(selectedIdx)
-    return [getCellKey(row, col - 1), selectedIdx, getCellKey(row, col + 1)]
+    return mapToCellKey([[row, col - 1], [row, col], [row, col + 1]])
 }
+
+const gliderSelector = (selectedIdx) => {
+    const [row, col] = rowColFromCelKey(selectedIdx)
+    return mapToCellKey([[row - 1, col - 1], [row + 1, col - 1], [row, col + 1], [row + 1, col], [row, col]])
+}
+
+const mapToCellKey = cellArrs => cellArrs.map((arr) => {
+    const [cellRow, cellCol] = arr
+    return getCellKey(cellRow, cellCol)
+})
