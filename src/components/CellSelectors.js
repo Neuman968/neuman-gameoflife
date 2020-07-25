7aa6d623724e8dc8836e4845450579780c4d4714
johnflynn <johnflynn493@gmail.com>
2020-07-25 19:09:48 -0400

    Added Glider Gun cell selector.

--- src/components/CellSelectors.js
+++ src/components/CellSelectors.js
@@ -12,6 +12,27 @@ export const gliderSelector = (selectedIdx) => {
     return mapToCellKey([[row - 1, col - 1], [row + 1, col - 1], [row, col + 1], [row + 1, col], [row, col]])
 }
 
+export const gliderGun = (selectedIdx) => {
+    const [row, col] = rowColFromCelKey(selectedIdx)
+    return mapToCellKey([
+        [row, col], [row, col + 1], [row + 1, col], [row + 1, col + 1],
+        [row, col + 10], [row + 1, col + 10], [row + 2, col + 10],
+        [row + 3, col + 11], [row + 4, col + 12], [row + 4, col + 13],
+        [row + 3, col + 15], [row + 2, col + 16], [row + 1, col + 16], [row + 1, col + 17], [row, col + 16],
+        [row + 1, col + 14], [row - 1, col + 15], [row - 2, col + 13], [row - 2, col + 12], [row - 1, col + 11],
+        [row, col + 20], [row - 1, col + 20], [row - 2, col + 20], [row, col + 21], [row - 1, col + 21], [row - 2, col + 21],
+        [row - 3, col + 22], [row - 3, col + 24], [row - 4, col + 24],
+        [row + 1, col + 22], [row + 1, col + 24], [row + 2, col + 24],
+        [row - 1, col + 34], [row - 2, col + 34], [row - 1, col + 35], [row - 2, col + 35]
+    ])
+}
+
+/**
+ * Utility function for converting [[row, col]...] to ['row-col'] cell key
+ * notation.
+ * @param cellArrs
+ * @returns {*}
+ */
 const mapToCellKey = cellArrs => cellArrs.map((arr) => {
     const [cellRow, cellCol] = arr
     return getCellKey(cellRow, cellCol)
