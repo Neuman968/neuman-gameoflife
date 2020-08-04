f68c626e83fa03ae6d1bc6ab2d4c9c2a45d00ba6
johnflynn <johnflynn493@gmail.com>
2020-08-03 22:11:31 -0400

    Removed redundant cell key destructuring

--- src/components/CellSelectors.js
+++ src/components/CellSelectors.js
@@ -2,42 +2,34 @@ import {mapToCellKey, rowColFromCelKey} from "./Simulation";
 
 export const dotSelector = (selectedIdx) => [selectedIdx]
 
-export const barSelector = (selectedIdx) => {
-    const [row, col] = rowColFromCelKey(selectedIdx)
-    return mapToCellKey([[row, col - 1], [row, col], [row, col + 1]])
-}
+export const barSelector = (selectedIdx) => selectorFunc(selectedIdx, (row, col) =>
+    mapToCellKey([[row, col - 1], [row, col], [row, col + 1]])
+)
 
-export const gliderSelector = (selectedIdx) => {
-    const [row, col] = rowColFromCelKey(selectedIdx)
-    return mapToCellKey([[row - 1, col - 1], [row + 1, col - 1], [row, col + 1], [row + 1, col], [row, col]])
-}
+export const gliderSelector = (selectedIdx) => selectorFunc(selectedIdx, (row, col) =>
+    mapToCellKey([[row - 1, col - 1], [row + 1, col - 1], [row, col + 1], [row + 1, col], [row, col]])
+)
 
-export const toadSelector = (selectedIdx) => {
-    const [row, col] = rowColFromCelKey(selectedIdx)
-    return mapToCellKey([[row, col], [row, col + 1], [row, col + 2],
+export const toadSelector = (selectedIdx) => selectorFunc(selectedIdx, (row, col) =>
+    mapToCellKey([[row, col], [row, col + 1], [row, col + 2],
         [row + 1, col - 1], [row + 1, col], [row + 1, col + 1]])
-}
+)
 
-export const lightWeightSpaceshipSelector = (selectedIdx) => {
-    const [row, col] = rowColFromCelKey(selectedIdx)
-    return mapToCellKey([[row, col], [row, col + 3], [row + 1, col - 1], [row + 2, col - 1], [row + 3, col - 1],
+export const lightWeightSpaceshipSelector = (selectedIdx) => selectorFunc(selectedIdx, (row, col) =>
+    mapToCellKey([[row, col], [row, col + 3], [row + 1, col - 1], [row + 2, col - 1], [row + 3, col - 1],
         [row + 3, col], [row + 3, col + 1], [row + 3, col + 2], [row + 2, col + 4]])
-}
+)
 
-export const acornSelector = (selectedIdx) => {
-    const [row, col] = rowColFromCelKey(selectedIdx)
-    return mapToCellKey([[row, col], [row + 2, col], [row + 2, col - 1], [row + 1, col + 2], [row + 2, col + 3],
+export const acornSelector = (selectedIdx) => selectorFunc(selectedIdx, (row, col) =>
+    mapToCellKey([[row, col], [row + 2, col], [row + 2, col - 1], [row + 1, col + 2], [row + 2, col + 3],
         [row + 2, col + 4], [row + 2, col + 5]])
-}
+)
 
-export const fPentominoSelector = (selectedIdx) => {
-    const [row, col] = rowColFromCelKey(selectedIdx)
-    return mapToCellKey([[row, col], [row, col + 1], [row + 1, col], [row + 1, col - 1], [row + 3, col]])
-}
+export const fPentominoSelector = (selectedIdx) => selectorFunc(selectedIdx, (row, col) =>
+    mapToCellKey([[row, col], [row, col + 1], [row + 1, col], [row + 1, col - 1], [row + 3, col]]))
 
-export const gliderGunSelector = (selectedIdx) => {
-    const [row, col] = rowColFromCelKey(selectedIdx)
-    return mapToCellKey([
+export const gliderGunSelector = (selectedIdx) => selectorFunc(selectedIdx, (row, col) =>
+    mapToCellKey([
         [row, col], [row, col + 1], [row + 1, col], [row + 1, col + 1],
         [row, col + 10], [row + 1, col + 10], [row + 2, col + 10],
         [row + 3, col + 11], [row + 4, col + 12], [row + 4, col + 13],
@@ -48,5 +40,9 @@ export const gliderGunSelector = (selectedIdx) => {
         [row + 1, col + 22], [row + 1, col + 24], [row + 2, col + 24],
         [row - 1, col + 34], [row - 2, col + 34], [row - 1, col + 35], [row - 2, col + 35]
     ])
-}
+)
 
+const selectorFunc = (selectedIdx, selectorFunc) => {
+    const [row, col] = rowColFromCelKey(selectedIdx)
+    return selectorFunc(row, col)
+}
